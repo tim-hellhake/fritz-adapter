8c190e6a981f8210626097118eab750ea0e895d5
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-01-03 01:34:23 +0100

    Use sha256sum instead of shasum

--- package.sh
+++ package.sh
@@ -2,15 +2,15 @@
 
 npm install --production
 
-shasum --algorithm 256 package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
-find node_modules -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+sha256sum package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 
 TARFILE=`npm pack`
 tar xzf ${TARFILE}
 cp -r node_modules ./package
 tar czf ${TARFILE} package
 
-shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+sha256sum ${TARFILE} > ${TARFILE}.sha256sum
 
 rm SHA256SUMS
 rm -rf package
